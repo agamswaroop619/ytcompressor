826e44fd7c37ae3ffdee44483a6405db30b0702c
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-02 11:23:42 +0530

    add: File Input

---
+++ src/app/components/file_input.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function file_input({}: Props) {
+  return (
+    <div>
+    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload file</label>
+    <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>        
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import File from "./components/file_input"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <File/>
     </main>
   )
 }
