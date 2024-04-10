33d7b0f1db69630b9dcf2961b26b8f736e3ce9d9
agamswaroop619 <agam.swaroop619@gmail.com>
2024-04-10 10:31:19 +0530

    add: Responsiveness changes

---
+++ src/app/components/form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function file_input({}: Props) {
+  return <div></div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import File from "./components/file_input"
+import File from "./components/file_input";
+import Form from "./components/form";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <File/>
+      <Form />
+      <File />
     </main>
-  )
+  );
 }
